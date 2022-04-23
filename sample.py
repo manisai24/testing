31eb016ae2fc9c070992314ead76a6b7090f8fdc
mani-saikkumorr.cheno <mani-saikkumorr.cheno@hpe.com>
2022-04-23 19:55:13 +0530

    harmonic series

--- sample.py
+++ sample.py
@@ -1,12 +1,10 @@
-# harmonic series using recursion
- 
 def sum(n):
  
     # Base condition
-    if n < 3:
+    if n < 2:
         return 1
  
     else:
-        return 1 / n + (sum(n - 2))
+        return 1 / n + (sum(n - 1))
          
-print(sum(9))
+print(sum(8))
